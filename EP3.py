fc57bf49b5cfeaf79cb534db8005f2c90f6ca94b
AIMVIEW <120380328+AIMVIEW@users.noreply.github.com>
2025-08-07 19:24:29 +0700

    Add files via upload

--- EP3.py
+++ EP3.py
@@ -2,6 +2,7 @@ import argparse
 import json
 from  pathlib import Path
 from datetime import datetime
+from tabulate import tabulate
 
 def load_expenses():
     file_path = Path('expenses.json')
@@ -28,11 +29,45 @@ def add_expense(description, amount):
     save_expenses(expenses)
     return new_id
 
+def delete_expense(id):
+    expenses = load_expenses()
+    initial_len = len(expenses)
+    expenses = [expense for expense in expenses if expense['id'] != id]
+    if len(expenses) < initial_len:
+        save_expenses(expenses)
+        return True
+    return False
+
+def list_expenses():
+    expenses = load_expenses()
+    if not expenses:
+        print("Not found expenses")
+        return
+    headers = ['ID', 'Date','Description','Amount']
+    rows = [[e['id'], e['date'],e['description'],f' {e['amount']:.2f}'] for e in expenses]
+    print(tabulate(rows, headers=headers, tablefmt='grid'))
+
+def summary_expenses():
+    expenses = load_expenses()
+    if not expenses:
+        print("Not found expenses")
+        return
+    total = sum(expense['amount'] for expense in expenses)
+    print(f'Total expenses: {total:.2f}')
+
 def main():
     parser = argparse.ArgumentParser(description='Expense Tracker')
-    parser.add_argument('command', choices=['add'], help = 'Command to execute')
-    parser.add_argument('--description', type = str, required=True, help='Description of expense')
-    parser.add_argument('--amount', type=float, required=True, help='Amont of expense')
+    subparsers = parser.add_subparsers(dest='command', required=True)
+
+    add_parser = subparsers.add_parser('add', help='Add a new expense')
+    add_parser.add_argument('--description', type = str, required=True, help='Description of expense')
+    add_parser.add_argument('--amount', type=float, required=True, help='Amont of expense')
+
+    delete_parser = subparsers.add_parser('delete', help='Delete expense by ID')
+    delete_parser.add_argument('--id', type = int, help='ID to delete')
+
+    summary_parser = subparsers.add_parser('summary', help='summary amount')
+    list_parser = subparsers.add_parser('list', help='show table each order')
     args = parser.parse_args()
 
     if args.command == 'add':
@@ -41,6 +76,19 @@ def main():
             return
         expense_id = add_expense(args.description, args.amount)
         print(f'Expense added successfully (ID: {expense_id})')
+
+    elif args.command == 'delete':
+        if not args.id:
+            print('Error: Id is required for delete')
+            return
+        elif delete_expense(args.id):
+            print(f'Expense deleted successfully (ID: {args.id})')
+        else:
+            print(f'Error: Expense with ID {args.id} not found')
+    elif args.command == 'list':
+        list_expenses()
+    elif args.command == 'summary':
+        summary_expenses()
 
 if __name__=='__main__':
     main()
